65321a2d5f560258ed3383522c8a41227cafcaa7
LaggerP <pablo_lagger@hotmail.com>
2019-11-13 19:29:10 -0300

    subida codigo modificado

--- src/main/main.java
+++ src/main/main.java
@@ -4,46 +4,52 @@ import java.util.Vector;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		// Ejercicio alineamiento de secuencias
 		String[] secuencia1 = {"A","C","C","C","G","A"};
 		String[] secuencia2 = {"T","C","C","T","A"};
-		
-		// 1 coincide, -1 no coincide, -2 gap
-		
-		alineamientoSecuencia(secuencia1, secuencia2);
+		crearSecuencia(secuencia1, secuencia2);
+
 	}
 
-	public static void alineamientoSecuencia(String[] secuencia1, String[] secuencia2) {
+	public static void crearSecuencia(String[] secuencia1, String[] secuencia2) {
 		int x = secuencia1.length;
 		int y = secuencia2.length;
-		
-		// damos el tamaño de la matriz
-		int matriz[][] = new int [x+1][y+1];
-		
-		
-		matriz[0][0] = 0;
-		for (int i = 1; i<x; i++) {
-			for (int j = 1; j<y; j++) {
-				
-				// si las letras coinciden
+
+		// damos el tamaÃ±o de la matriz
+		int matriz[][] = new int [x][y];
+
+		for (int i = 0; i<x; i++) {
+			for (int j = 0; j<y; j++) {
+				// 1 si las letras coinciden
 				if (secuencia1[i].compareTo(secuencia2[j]) == 0) {
 					matriz[i][j] = 1;
-				} 
-				// si las letras no coinciden y no hay espacios
+				}
+				// -1 si las letras no coinciden y no hay espacios
 				else if (secuencia1[i].compareTo(secuencia2[j]) != 0 && secuencia2[j] != " ") {
 					matriz[i][j] = -1;
 				}
-				// si hay espacios
-				else 
+				// -2 si hay espacios
+				else
 					matriz[i][j] = -2;
 				}
 			}
+
+		for (int i = 0; i<x; i++) {
+			System.out.print("|");
+			for (int j = 0; j < y; j++) {
+				System.out.print(" ");
+				System.out.print(matriz[i][j]);
+
+
+			}
+			System.out.println("\t");
 		}
-		
-		
-		
+		// buscarMejorAlineamiento(matriz);
+		}
+
+	// este metodo realiza la busqueda del mejor alineamiento.
+	private static void buscarMejorAlineamiento(int[][] matriz) {
+
 	}
-
-
 }
